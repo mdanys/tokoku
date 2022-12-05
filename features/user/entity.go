e5d6e3a92210c5c0b07aaeaac07bade34a9e00d3
mdanys <mochammaddany@gmail.com>
2022-12-06 00:53:54 +0700

    add logic.go

--- features/user/entity.go
+++ features/user/entity.go
@@ -16,12 +16,12 @@ type Repository interface {
 	Insert(data Core) (Core, error)
 	Edit(data Core, id uint) (Core, error)
 	Remove(id uint) error
-	Login(data Core) (Core, error)
+	Login(input Core) (Core, error)
 }
 
 type Service interface {
 	Create(data Core) (Core, error)
 	Update(data Core, id uint) (Core, error)
 	Delete(id uint) error
-	Login(data Core) (Core, error)
+	Login(input Core) (Core, error)
 }
