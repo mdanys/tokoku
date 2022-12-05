3f40ef93f2a2ecdb83c58d1de4ab5e77d89f0c95
mdanys <mochammaddany@gmail.com>
2022-12-05 17:23:32 +0700

    setup entity.go

--- features/user/entity.go
+++ features/user/entity.go
@@ -1,1 +1,35 @@
 package user
+
+type Core struct {
+	ID       uint
+	Name     string
+	Phone    string
+	Email    string
+	Password string
+	Product  []ProductCore
+}
+
+type ProductCore struct {
+	ID     uint
+	UserID uint
+	Name   string
+	Image  string
+	Qty    uint
+	Price  uint
+}
+
+type Repository interface {
+	Insert(data Core) (Core, error)
+	Edit(data Core, id uint) (Core, error)
+	Remove(id uint) error
+	GetMyProfile(token uint) (Core, error)
+	GetByID(id uint) (Core, error)
+}
+
+type Service interface {
+	Create(data Core) (Core, error)
+	Update(data Core, id uint) (Core, error)
+	Delete(id uint) error
+	ShowMyProfile(token uint) (Core, error)
+	GetByID(id uint) (Core, error)
+}
